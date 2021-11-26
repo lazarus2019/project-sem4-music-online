8b1bcc65c0d0a4b4e065942fcd8454d78f4ca15c
VietNguyen <vietnguyen0444@gmail.com>
2021-11-26 23:49:43 +0700

    test 8

---
+++ src/main/java/com/demo/controllers/user/NewController.java
@@ -0,0 +1,24 @@
+package com.demo.controllers.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.demo.models.AlbumInfo;
+
+@Controller
+@RequestMapping(value = { "notification" })
+public class NewController {
+
+	// AlbumInfo albumInfo = new AlbumInfo();
+
+	@RequestMapping(value = { "", "index" })
+	public String index() {
+		
+		System.out.println("them 1 dong moi nhaaaaaaaaa");
+		return "notification/index";
+
+	}
+
+	// ahihihihi
+
+}
