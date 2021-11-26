6a1d76d42ddffcbd15834acf56d26ceb5dfcedc7
VietNguyen <vietnguyen0444@gmail.com>
2021-11-26 23:18:29 +0700

    test 5

--- src/main/java/com/demo/controllers/user/NotificationController.java
+++ src/main/java/com/demo/controllers/user/NotificationController.java
@@ -6,18 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.demo.models.AlbumInfo;
 
 @Controller
-@RequestMapping(value = { "notification"} )
+@RequestMapping(value = { "notification" })
 public class NotificationController {
-	
-	//AlbumInfo albumInfo = new AlbumInfo();
-	
-	  @RequestMapping( value = { "","index" }) public String index() { return
-	  "notification/index" ;
-	  
-	  }
-	 
-	//ahihihihi
-	
+
+	// AlbumInfo albumInfo = new AlbumInfo();
+
+	@RequestMapping(value = { "", "index" })
+	public String index() {
+		System.out.println("aaaaaaaaaaaaa");
+		System.out.println("bbbbbbbbbbbbbbbbs");
+		
+		
+		System.out.println("ccccccccccccccc");
+		return "notification/index";
+
+	}
+
+	// ahihihihi
+
 }
-
-
