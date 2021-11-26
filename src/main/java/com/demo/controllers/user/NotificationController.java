faeb77de240c97fc22d5d4b3b7a613229562fce5
VietNguyen <vietnguyen0444@gmail.com>
2021-11-26 23:13:00 +0700

    test 3

--- src/main/java/com/demo/controllers/user/NotificationController.java
+++ src/main/java/com/demo/controllers/user/NotificationController.java
@@ -3,10 +3,13 @@ package com.demo.controllers.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.demo.models.AlbumInfo;
+
 @Controller
 @RequestMapping(value = { "notification"} )
 public class NotificationController {
-
+	
+	AlbumInfo albumInfo = new AlbumInfo();
 	/*
 	 * @RequestMapping( value = { "","index" }) public String index() { return
 	 * "notification/index" ;
@@ -14,4 +17,7 @@ public class NotificationController {
 	 * }
 	 */
 	//ahihihihi
+	
 }
+
+
